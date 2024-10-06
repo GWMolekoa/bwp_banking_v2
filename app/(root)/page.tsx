853121571406855c6139a6288f3b65265af78f14
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-10-06 15:18:12 +0200

    Create A HeaderBox Component and Page. Modify TailwindConfig.ts Indentation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+
 
 const Home = () => {
+  const loggedIn = {firstName: "Wilson"};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access & Manage Your Account & Transactions Efficiently"
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
