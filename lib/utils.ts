5ec1926cf3217ebbf6f2951cf0ecb2a3b51e3640
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-10-07 01:39:53 +0200

    Implement AuthForm - Sign-In

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,7 @@ import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
 import CountUp from "react-countup";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -194,3 +195,9 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+// FormSchema Extraction
+export const authFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+  })
