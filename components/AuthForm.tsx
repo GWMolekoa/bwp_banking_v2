2975bfd4c2e09ac18a5a7f98ab4787a93c55d28d
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-10-07 13:35:02 +0200

    Implement AuthForms (SignIn/SignUp), ServerSide Actions for Auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,14 +22,19 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ( {type}: {type: string }) => {
+    const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
+    const formSchema = authFormSchema(type);
+
     // 1. Define your form.
-    const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+    const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: ""
@@ -37,14 +42,31 @@ const AuthForm = ( {type}: {type: string }) => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    setIsLoading(true)
-    console.log(values)
-    setIsLoading(false);
-  }
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
 
+    try {
+        // Sign Up With Appwrite and Create Plaid-Token
+        // First form correct user data to sign up in plaid and appwrite
+        if(type === 'sign-up') {
+            const newUser = await signUp(data);
+
+            setUser(newUser);
+        }
+
+        if(type === 'sign-in') {
+           const response = await signIn ({
+               email: data.email,
+               password: data.password,
+           })
+           if(response) router.push('/')
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        setIsLoading(false);
+    }
+  }
 
   return (
     <section className='auth-form'>
@@ -89,6 +111,69 @@ const AuthForm = ( {type}: {type: string }) => {
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            {type === 'sign-up' && (
+                                <>
+
+                                    <div className='flex gap-4'>
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='firstName'
+                                            label='First Name'
+                                            placeholder='Enter Your First Name'
+                                        />
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='lastName'
+                                            label='Last Name'
+                                            placeholder='Enter Your Last Name'
+                                        />
+                                    </div>
+                                    
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='address1'
+                                            label='Address'
+                                            placeholder='Enter Your Specific Address'
+                                        />
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='city'
+                                            label='City'
+                                            placeholder='Enter Your City'
+                                        />
+
+                                    <div className='flex gap-4'>
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='state'
+                                            label='State'
+                                            placeholder='Example: NY, GA or TX etc.'
+                                        />
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='postalCode'
+                                            label='Postal Code'
+                                            placeholder='Example: 01234 '
+                                        />                             
+                                    </div>    
+                                        
+                                    <div className='flex gap-4'>
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='dateOfBirth'
+                                            label='Date of Birth'
+                                            placeholder='YYYY-MM-DD'
+                                        />
+                                        <CustomInput 
+                                            control= {form.control}
+                                            name='ssn'
+                                            label='SSN'
+                                            placeholder='Example: 123456789'
+                                        />  
+                                    </div>    
+                         
+                                </>
+                            )}
 
                             <CustomInput 
                                 control= {form.control}
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server'
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database / Make a Fetch
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Mutation / Database / Make a Fetch - Use Appwrite to Create A User Account
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
